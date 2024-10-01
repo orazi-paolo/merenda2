55730fecc310be1592c9b100f945917a9cc8f46a
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-01 18:25:41 +0200

    completed snack

--- merenda.js
+++ merenda.js
@@ -1,3 +1,9 @@
 function rimuoviDuplicati(arr) {
-    // A voi!
+    // uso la funzione set perchè non permette duplicati
+    let noDuplicates = new Set(arr);
+    // controllo che valore restituisce set
+    console.log(noDuplicates)
+    // ritorno l'array dei non duplicati
+    // prima pero devo riportare il set ad un array con lo spread operator
+    return [...noDuplicates]
 }
